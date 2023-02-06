4a6dddbd852102eda39806a871b9fbc762700daa
Swifthing <hannyce@icloud.com>
2023-02-06 19:13:43 +0400

    init xcode project

---
+++ GoogleBookAPI/GoogleBookAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleBookAPIApp.swift
+//  GoogleBookAPI
+//
+//  Created by Anis on 06/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct GoogleBookAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
